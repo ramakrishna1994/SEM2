14da3f1e46b4bd12397dc01d2bc148749c8a155a
saradhi <ramakrishnasaradhi@gmail.com>
2018-01-20 11:58:54 +0530

    done

--- AI/Assg1/AI_CHATBOT.py
+++ AI/Assg1/AI_CHATBOT.py
@@ -3,6 +3,7 @@ import requests
 import aiml
 import os
 import json
+from pkg_resources._vendor.pyparsing import empty
 
 
 kernel = aiml.Kernel()
@@ -11,7 +12,7 @@ WEATHER_API_CALL_FAILED = "weather api call failed"
 
 # Function definition is here
 def callweatherapi( query ):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q='+query+'&appid='+API_KEY
+    url = 'http://api.openweathermap.org/data/2.5/weather?q='+query+'&appid='+API_KEY+'&units=metric'
     response = requests.get(url)
     if(response.status_code == 200):
         x = response.content
@@ -24,11 +25,34 @@ def printtemperature(answer):
     newquery = kernel.respond(answer)
     data = callweatherapi(newquery)
     if data != WEATHER_API_CALL_FAILED:
-        temp = data['main']['temp']-273
-        result = 'Temperature in ' +newquery +' is '+str(temp) +' celsius'
+        temp = data['main']['temp']
+        maxtemp = data['main']['temp_max']
+        result = 'Temperature in ' +newquery +' is currently '+str(temp) +' celsius and it can reach upto '+ str(maxtemp) +' celsius. Take Care !!'
         return result
     else :
-        return 'Can you be a bit more specific!!'
+       return 'Can you be a bit more specific!! I am not able to find city name'
+    
+def printhumidity(answer):
+    newquery = kernel.respond(answer)
+    data = callweatherapi(newquery)
+    if data != WEATHER_API_CALL_FAILED:
+        humd = data['main']['humidity']
+        result = 'Humidity in ' +newquery +' is currently '+str(humd) +'%'
+        return result
+    else :
+        return 'Can you be a bit more specific!! I am not able to find city name'
+    
+def printweather(answer):
+    newquery = kernel.respond(answer)
+    data = callweatherapi(newquery)
+    if data != WEATHER_API_CALL_FAILED:
+        weath = data['weather'][0]['description']
+        humd = data['main']['humidity']
+        temp = data['main']['temp']
+        result = 'Current Weather in ' +newquery +' : '+ str(weath) + ' with temperature '+str(temp) + ' celsius and humidity ' +str(humd)+'%'
+        return result
+    else :
+        return 'Can you be a bit more specific!! I am not able to find city name'
     
 # Create the kernel and learn AIML files
 kernel = aiml.Kernel()
@@ -49,6 +73,12 @@ while True:
         answer = kernel.respond(query)
         if "temperature" in answer:
             print "BOT(ANSWER)     >> " + printtemperature(answer)
+        elif "humidity" in answer:
+            print "BOT(ANSWER)     >> " + printhumidity(answer)
+        elif "weather" in answer:
+            print "BOT(ANSWER)     >> " + printweather(answer)
+        elif answer == "":
+            continue
         else:
             print "BOT(ANSWER)     >> " + answer
             
